51625c4441c830d12e0aa4a926941c191cfbbf26
twindai <guandai@hotmail.com>
2025-02-21 08:03:48 +0100

    Initial Commit

---
+++ TxtToPdf/TxtToPdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  TxtToPdfApp.swift
+//  TxtToPdf
+//
+//  Created by zheng dai on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TxtToPdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TxtToPdfTests/TxtToPdfTests.swift
@@ -0,0 +1,16 @@
+//
+//  TxtToPdfTests.swift
+//  TxtToPdfTests
+//
+//  Created by zheng dai on 2/21/25.
+//
+
+import Testing
+
+struct TxtToPdfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
