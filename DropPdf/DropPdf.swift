130ee7eda3e93b90947f5fc72e65873a6e2a2d20
twindai <guandai@hotmail.com>
2025-02-25 10:58:22 +0100

    single instance

--- DropPdf/DropPdf.swift
+++ DropPdf/DropPdf.swift
@@ -4,15 +4,36 @@ import SwiftUI
 struct MyApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
+    init() {
+        ensureSingleInstance()
+    }
+
     var body: some Scene {
-        // 1) Give the window a unique "id:"
+        sceneContent
+    }
+
+    @SceneBuilder
+    var sceneContent: some Scene {
         WindowGroup("Drop To PDF", id: "MainWindow") {
             ContentView()
                 .environmentObject(appDelegate)
         }
-        // 2) Force ALL external events (file opens) to reuse this scene
         .handlesExternalEvents(matching: ["*"])
         .defaultSize(width: 250, height: 250)
         .windowResizability(.contentSize)
+        .commands {
+            CommandGroup(replacing: .newItem) { } // Hide "New Window" option
+        }
+    }
+
+    /// Ensures that only a single instance of the app runs
+    private func ensureSingleInstance() {
+        let bundleIdentifier = Bundle.main.bundleIdentifier!
+        let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier)
+
+        if runningApps.count > 1 {
+            // If another instance exists, terminate the current one
+            NSApplication.shared.terminate(nil)
+        }
     }
 }
