567c519528b351107ca86fb1865faf8f8f2576a6
twindai <guandai@hotmail.com>
2025-03-25 07:20:02 +0100

    add test

---
+++ DropPdfTests/DropPdfTests2.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  DropPdf
+//
+//  Created by zheng dai on 3/25/25.
+//
+
---
+++ DropPdfUnitTests/DropPdfUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  DropPdfUnitTests.swift
+//  DropPdfUnitTests
+//
+//  Created by zheng dai on 3/25/25.
+//
+
+import Testing
+
+struct DropPdfUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
