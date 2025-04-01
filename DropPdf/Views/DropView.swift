4a0627d9e4809eabb1daefc7717c99fffaecbecb
twindai <guandai@hotmail.com>
2025-04-01 11:19:04 +0200

    update view

--- DropPdf/Views/DropView.swift
+++ DropPdf/Views/DropView.swift
@@ -8,6 +8,7 @@ struct DropView: View {
     @State private var systemName = "checkmark.circle.fill"
     @State private var systemColor: Color = .green
     @State private var showPanel = false
+    @State private var createOneFile = false
 
     static let baseSize: CGFloat = 80  // ✅ Use `static let`
     static let dropAreaLength: CGFloat = 2.8 * DropView.baseSize
@@ -16,72 +17,95 @@ struct DropView: View {
     static let resultsLength: CGFloat = 6 * DropView.baseSize
     static let historyLength: CGFloat = 4.5 * DropView.baseSize
     
-    var body: some View {
-        VStack(spacing: 12) {
-            HStack {
-                Text("Drop files here")
-                    .font(.headline)
-                    .foregroundColor(isDragging ? .blue : .secondary)
+    var BoxSign : some View {
+        VStack {
+            Image(systemName: "tray.and.arrow.down.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: DropView.baseSize, height: DropView.baseSize)
+                .foregroundColor(isDragging ? .blue : .gray)
 
-                Button(action: {
-                    showPanel = true
-                }) {
-                    Image(systemName: "info.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(.blue)
-                }
-                .buttonStyle(PlainButtonStyle()) // No extra styling
-                .accessibilityIdentifier("infoButton")
-            }
-            .padding(.top, 10)
-
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.2), Color.purple.opacity(0.3)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(isDragging ? Color.blue : Color.gray.opacity(0.5), lineWidth: 3)
-                    )
-                    .shadow(color: isDragging ? Color.blue.opacity(0.5) : Color.gray.opacity(0.3), radius: 10, x: 0, y: 5)
-                    .frame(width: DropView.dropAreaLength, height: DropView.dropAreaLength)
-
-
-                if showMark {
-                    Image(systemName: systemName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: DropView.iconLength, height: DropView.iconLength)
-                        .foregroundColor(systemColor)
-                        .opacity(showMark ? 1.0 : 0.75)
-                        .animation(.easeIn(duration: 0.2), value: showMark)
-                        .onAppear {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                                withAnimation(.easeOut(duration: 0.2)) {
-                                    showMark = false
-                                }
-                            }
-                        }
-                } else {
-                    VStack {
-                        Image(systemName: "tray.and.arrow.down.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: DropView.baseSize, height: DropView.baseSize)
-                            .foregroundColor(isDragging ? .blue : .gray)
-
-                        Text("Drag & Drop")
-                            .font(.body)
-                            .foregroundColor(.primary)
+            Text("Drag & Drop")
+                .font(.body)
+                .foregroundColor(.primary)
+        }
+    }
+    
+    var CheckResult: some View { Image(systemName: systemName)
+            .resizable()
+            .scaledToFit()
+            .frame(width: DropView.iconLength, height: DropView.iconLength)
+            .foregroundColor(systemColor)
+            .opacity(showMark ? 1.0 : 0.75)
+            .animation(.easeIn(duration: 0.2), value: showMark)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation(.easeOut(duration: 0.2)) {
+                        showMark = false
                     }
                 }
             }
-            .onDrop(of: ["public.file-url"], isTargeted: $isDragging) { providers in
-                handleDrop(providers)
+    }
+    
+    var DropBox : some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.2), Color.purple.opacity(0.3)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(isDragging ? Color.blue : Color.gray.opacity(0.5), lineWidth: 3)
+                )
+                .shadow(color: isDragging ? Color.blue.opacity(0.5) : Color.gray.opacity(0.3), radius: 10, x: 0, y: 5)
+                .frame(width: DropView.dropAreaLength, height: DropView.dropAreaLength)
+
+            if showMark {
+                CheckResult
+            } else {
+                BoxSign
             }
         }
-        .frame(width: DropView.outerLength, height: DropView.outerLength)
+        .onDrop(of: ["public.file-url"], isTargeted: $isDragging) { providers in
+            handleDrop(providers)
+        }
+    }
+    
+    var ProcessedFilesBtn: some View {
+        Button(action: {
+                showPanel = true
+            }) {
+                Image(systemName: "info.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 15, height: 15)
+                    .foregroundColor(.blue)
+            }
+            .buttonStyle(PlainButtonStyle()) // No extra styling
+            .accessibilityIdentifier("infoButton")
+    }
+    
+    var TopBar: some View {
+        HStack {
+            Toggle(isOn: $createOneFile) {}
+            .toggleStyle(SwitchToggleStyle())
+            .accessibilityIdentifier("switchButton")
+            .padding(.leading, 4)
+
+            // Show the on/off result as text
+            Text(createOneFile ? "Create A Bundle" : "Create Separate")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.leading, 8)
+        }
+        .padding(.top, 10)
+    }
+    
+    var WrapperMainBox : some View {
+        ZStack {
+            VStack(spacing: 12) {
+                TopBar
+                DropBox
+            }
+        }
         .padding(10)
         .padding(.bottom, 20)
         .background(
@@ -89,9 +113,29 @@ struct DropView: View {
                 .fill(getBackgroundColor())
                 .shadow(radius: 10)
         )
-        .sheet(isPresented: $showPanel) {
-            ProcessedFilesPanel(processedFiles: appDelegate.processResult, isPresented: $showPanel)
+    }
+    
+    var WrapperProcessBtn : some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                ProcessedFilesBtn
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 20)
+                    .sheet(isPresented: $showPanel) {
+                       ProcessedFilesPanel(processedFiles: appDelegate.processResult, isPresented: $showPanel)
+                    }
+            }
         }
+    }
+
+    var body: some View {
+        ZStack {
+            WrapperMainBox
+            WrapperProcessBtn
+        }
+        .frame(width: DropView.outerLength + 10, height: DropView.outerLength + 40)
     }
 
     /// Determines the correct background color for iOS and macOS
@@ -134,62 +178,5 @@ struct DropView: View {
         }
 
         return true
-    }
-}
-
-
-struct ProcessedFilesPanel: View {
-    let processedFiles: [Int: (URL, Bool)]
-    @Binding var isPresented: Bool
-
-    var body: some View {
-        VStack {
-            Text("Processed Files")
-                .font(.headline)
-                .padding(5)
-
-            ScrollView {
-                LazyVStack {
-                    // 1) Sort the dictionary keys so we have a stable order
-                    ForEach(processedFiles.keys.sorted(), id: \.self) { index in
-                        if let (a, b) = processedFiles[index] {
-                            HStack {
-                                Text(a.lastPathComponent)
-                                    .font(.headline)
-                                Spacer()
-                                Text(b ? "Success" : "Failed")
-                                    .foregroundColor(b ? .green : .red)
-                            }
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 2)
-                            .background(index % 2 == 1 ? Color.gray.opacity(0.2) : Color.clear) // Light grey for odd indices
-                        }
-                    }
-                }
-                .id(UUID())
-            }
-            .frame(height: DropView.historyLength)
-            .background(Color(NSColor.windowBackgroundColor))
-            .clipShape(RoundedRectangle(cornerRadius: 5))
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-            )
-            .padding(.horizontal, 10)
-
-            Button("CLOSE") {
-                isPresented = false
-            }
-            .padding(.top, 0)
-            .font(.system(size: 14))
-            .frame(width: 80, height: 30)
-            .background(Color.blue)
-            .cornerRadius(6)
-            .padding(.top, 0)
-            .buttonStyle(.plain)
-            .accessibilityIdentifier("closeButton") 
-            
-        }
-        .frame(width: DropView.resultsLength, height: DropView.resultsLength)
     }
 }
---
+++ DropPdf/Views/ProcessedFilesPanel.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+struct ProcessedFilesPanel: View {
+    let processedFiles: [Int: (URL, Bool)]
+    @Binding var isPresented: Bool
+
+    var body: some View {
+        VStack {
+            Text("Processed Files")
+                .font(.headline)
+                .padding(5)
+
+            ScrollView {
+                LazyVStack {
+                    // 1) Sort the dictionary keys so we have a stable order
+                    ForEach(processedFiles.keys.sorted(), id: \.self) { index in
+                        if let (a, b) = processedFiles[index] {
+                            HStack {
+                                Text(a.lastPathComponent)
+                                    .font(.headline)
+                                Spacer()
+                                Text(b ? "Success" : "Failed")
+                                    .foregroundColor(b ? .green : .red)
+                            }
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 2)
+                            .background(index % 2 == 1 ? Color.gray.opacity(0.2) : Color.clear) // Light grey for odd indices
+                        }
+                    }
+                }
+                .id(UUID())
+            }
+            .frame(height: DropView.historyLength)
+            .background(Color(NSColor.windowBackgroundColor))
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+            )
+            .padding(.horizontal, 10)
+
+            Button("CLOSE") {
+                isPresented = false
+            }
+            .padding(.top, 0)
+            .font(.system(size: 14))
+            .frame(width: 80, height: 30)
+            .background(Color.blue)
+            .cornerRadius(6)
+            .padding(.top, 0)
+            .buttonStyle(.plain)
+            .accessibilityIdentifier("closeButton")
+            
+        }
+        .frame(width: DropView.resultsLength, height: DropView.resultsLength)
+    }
+}
