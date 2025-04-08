c6f4988c31afc2e3a17b7aac397a6004a480cf77
twindai <guandai@hotmail.com>
2025-04-08 10:42:36 +0200

    update code

--- DropPdf/bin/sign.sh
+++
@@ -1,5 +0,0 @@
-export PUBLISH_KEY=4116EBC16ACE4702F6C509D3B3385B9AF6D8EFAB
-codesign --force --sign ${PUBLISH_KEY} --entitlements ./DropPDF/bin/bin.entitlements ./DropPDF/bin/antiword  \
-&& codesign --force --sign ${PUBLISH_KEY} --entitlements ./DropPDF/bin/bin.entitlements ./DropPDF/bin/catdoc  \
-&& codesign --force --sign ${PUBLISH_KEY} --entitlements ./DropPDF/bin/bin.entitlements ./DropPDF/bin/soffice  \
-&& ./DropPdf/bin/soffice   /Users/zhengdai/Downloads/doc.doc 
